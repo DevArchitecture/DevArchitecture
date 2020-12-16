3500048c0b80f0295cacc7feeab5f7ad79c2bce8
Kerem VARIŞ <webyonett@gmail.com>
2020-12-16 17:58:09 +0300

    add example class

---
+++ Entities/Concrete/DocumentDbEntityExample.cs
@@ -0,0 +1,8 @@
+﻿using Core.DataAccess.Concrete;
+
+namespace Entities.Concrete
+{
+	public class DocumentDbEntityExample : DocumentDbEntity
+	{
+	}
+}
--- Entities/Concrete/EntityExample.cs
+++ Entities/Concrete/EntityExample.cs
@@ -1,11 +1,8 @@
 ﻿using Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Entities.Concrete
 {
-	public class Example : IEntity
+	public class EntityExample : IEntity
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
