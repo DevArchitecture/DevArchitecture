8e92e48616e1383991e8265f5f5780ec9de69273
Kerem VARIŞ <keremvaris@hotmail.com.tr>
2020-12-28 18:09:35 +0300

    program cs edited

--- UiPreparation/Program.cs
+++ UiPreparation/Program.cs
@@ -13,12 +13,9 @@ namespace UiPreparation
 			var deletePath = exePath + @"\node_modules\selenium-webdriver\lib\test\data";
 			StringBuilder bld = new StringBuilder();
 
-			//bld.Append("npm install --save-dev @angular/cli@latest&");
-			//bld.Append("ng update @angular/core @angular/cli&");
-			//bld.Append("ng update @angular/material&");
+			bld.Append("npm install --save-dev @angular/cli@latest&");
 			bld.Append("npm install&");
 			bld.Append("npm install popper.js --save&");
-			bld.Append("npm prune&");
 			bld.Append("code .&");
 			bld.Append($"RD /S /Q {deletePath} &");
 			bld.Append("ng serve --open&");			
