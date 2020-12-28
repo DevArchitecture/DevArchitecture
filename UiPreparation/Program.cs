71bdc2c8792b7ea8a0db3940a3628c790ca6b1ae
Kerem VARIŞ <keremvaris@hotmail.com.tr>
2020-12-28 17:29:40 +0300

    npm packages

--- UiPreparation/Program.cs
+++ UiPreparation/Program.cs
@@ -13,9 +13,9 @@ namespace UiPreparation
 			var deletePath = exePath + @"\node_modules\selenium-webdriver\lib\test\data";
 			StringBuilder bld = new StringBuilder();
 
-			bld.Append("npm install --save-dev @angular/cli@latest&");
-			bld.Append("ng update @angular/core @angular/cli&");
-			bld.Append("ng update @angular/material&");
+			//bld.Append("npm install --save-dev @angular/cli@latest&");
+			//bld.Append("ng update @angular/core @angular/cli&");
+			//bld.Append("ng update @angular/material&");
 			bld.Append("npm install&");
 			bld.Append("npm install popper.js --save&");
 			bld.Append("npm prune&");
