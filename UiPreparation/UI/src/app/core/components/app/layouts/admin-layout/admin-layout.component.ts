f7b4f3b6749ba241d6e71bfcb15eda44ffc99482
Kerem VARIŞ <keremvaris@hotmail.com.tr>
2021-01-12 22:36:18 +0300

    perfectscroolbar

--- UiPreparation/UI/src/app/core/components/app/layouts/admin-layout/admin-layout.component.ts
+++ UiPreparation/UI/src/app/core/components/app/layouts/admin-layout/admin-layout.component.ts
@@ -67,10 +67,10 @@ export class AdminLayoutComponent implements OnInit {
                 elemMainPanel.scrollTop = 0;
                 elemSidebar.scrollTop = 0;
             });
-            // if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
-            //     let ps = new PerfectScrollbar(elemMainPanel);
-            //     ps = new PerfectScrollbar(elemSidebar);
-            // }
+             if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+                 let ps = new PerfectScrollbar(elemMainPanel);
+                 ps = new PerfectScrollbar(elemSidebar);
+             }
 
             const window_width = $(window).width();
             let $sidebar = $('.sidebar');
