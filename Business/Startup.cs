89cc4b3b2f37a7fe34f5426a449436bf7b75b6c7
Murat Kurtboğan <murat.kurtbogan@odeontechnology.com>
2021-01-31 22:03:25 +0300

    Mongo

--- Business/Startup.cs
+++ Business/Startup.cs
@@ -13,6 +13,8 @@ using Core.Utilities.Security.Jwt;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.EntityFramework.Contexts;
+using DataAccess.Concrete.MongoDb;
+using DataAccess.Concrete.MongoDb.Collections;
 using DataAccess.Concrete.MongoDb.Context;
 using FluentValidation;
 using MediatR;
