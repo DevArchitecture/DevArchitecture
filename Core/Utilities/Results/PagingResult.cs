a474f0eaf1e372a4749007605382133d1505a7a1
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-09-18 11:26:16 +0300

    pagination return type added

---
+++ Core/Utilities/Results/PagingResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Utilities.Results
+{
+    /// <summary>
+    /// Paginated response
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PagingResult<T> : Result, IPagingResult<T>
+    {
+        public PagingResult(List<T> data, int totalItemCount, bool success, string message) : base(success, message)
+        {
+            Data = data;
+            TotalItemCount = totalItemCount;
+        }
+
+        public List<T> Data { get; }
+        public int TotalItemCount { get; }
+    }
+}
