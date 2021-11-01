cc3e7dc2648e925b9eaca4d067965fc0d28ddc3f
Huseyinb <huseyinberber20@gmail.com>
2021-11-01 16:56:35 +0300

    DevArchitecture BDD (Specflow) added.

---
+++ DevArchitecture.Specs/Helpers/MockJwtTokens.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DevArchitecture.Specs.Helpers
+{
+    public static class MockJwtTokens
+    {
+        private static readonly JwtSecurityTokenHandler STokenHandler = new();
+        private static string _keyString = "!z2x3C4v5B*_*!z2x3C4v5B*_*";
+
+        static MockJwtTokens()
+        {
+            var SKey = Encoding.UTF8.GetBytes(_keyString);
+            SecurityKey = new SymmetricSecurityKey(SKey);
+            SigningCredentials = new SigningCredentials(SecurityKey, SecurityAlgorithms.HmacSha256Signature);
+        }
+
+        public static string Issuer { get; } = "www.devarchitecture.com";
+        public static string Audience { get; } = "www.devarchitecture.com";
+        public static SecurityKey SecurityKey { get; }
+        public static SigningCredentials SigningCredentials { get; }
+
+        public static string GenerateJwtToken(IEnumerable<Claim> claims, double value = 5)
+        {
+            return STokenHandler.WriteToken(new JwtSecurityToken(Issuer, Audience, claims, DateTime.UtcNow, DateTime.UtcNow.AddSeconds(value), SigningCredentials));
+        }
+    }
+}
---
+++ DevArchitecture.Specs/Hooks/Hooks.cs
@@ -0,0 +1,58 @@
+﻿
+using Autofac.Extensions.DependencyInjection;
+using Core.CrossCuttingConcerns.Caching;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TechTalk.SpecFlow;
+using WebAPI;
+
+namespace DevArchitecture.Specs.Hooks
+{
+    [Binding, Scope(Feature = "Users")]
+    public sealed class Hooks
+    {
+        private static IHost _host;
+
+        [BeforeFeature]
+        public static void BeforeFeature()
+        {
+            var configuration = new ConfigurationBuilder()
+              .SetBasePath(@"C:\Users\HUS\source\repos\DevArchitecture\WebAPI")
+              .AddEnvironmentVariables()
+              .AddJsonFile("appsettings.json", optional: true)
+              .AddJsonFile("appsettings.Development.json", optional: true)
+              .Build();
+
+            _host = new HostBuilder()
+            .UseContentRoot(@"C:\Users\HUS\source\repos\DevArchitecture\WebAPI")
+            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+            .UseEnvironment("Development")
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddMemoryCache();
+                services.AddSingleton<ICacheManager, Core.CrossCuttingConcerns.Caching.Microsoft.MemoryCacheManager>();
+            })
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.Sources.Clear();
+                builder.AddConfiguration(configuration);
+            })
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(LogLevel.Trace);
+            }).Build();
+            _host.StartAsync();
+        }
+
+        [AfterFeature]
+        public static void AfterScenario()
+        {
+            _host.StopAsync();
+        }
+    }
+}
