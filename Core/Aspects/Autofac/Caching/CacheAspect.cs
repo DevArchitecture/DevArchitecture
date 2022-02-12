1dbe52fc5d11f170cf92056b53911ce5a096d36f
Kerem VARIŞ <keremvaris@hotmail.com.tr>
2022-02-12 21:09:25 +0300

    Revert "."
    This reverts commit d9a60040a13d1e2fcaf1f448a0b90c4d3228438b.

--- Core/Aspects/Autofac/Caching/CacheAspect.cs
+++ Core/Aspects/Autofac/Caching/CacheAspect.cs
@@ -8,47 +8,47 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Core.Aspects.Autofac.Caching
 {
-  /// <summary>
-  /// CacheAspect
-  /// </summary>
-  public class CacheAspect : MethodInterception
-  {
-    private readonly int _duration;
-    private readonly ICacheManager _cacheManager;
-
-    public CacheAspect(int duration = 60)
+    /// <summary>
+    /// CacheAspect
+    /// </summary>
+    public class CacheAspect : MethodInterception
     {
-      _duration = duration;
-      _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        private readonly int _duration;
+        private readonly ICacheManager _cacheManager;
+
+        public CacheAspect(int duration = 60)
+        {
+            _duration = duration;
+            _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var methodName = string.Format($"{invocation.Arguments[0]}.{invocation.Method.Name}");
+            var arguments = invocation.Arguments;
+            var key = $"{methodName}({BuildKey(arguments)})";
+            if (_cacheManager.IsAdd(key))
+            {
+                invocation.ReturnValue = _cacheManager.Get(key);
+                return;
+            }
+
+            invocation.Proceed();
+            _cacheManager.Add(key, System.Text.Json.JsonSerializer.Serialize(invocation.ReturnValue), _duration);
+        }
+
+
+        string BuildKey(object[] args)
+        {
+            var sb = new StringBuilder();
+            foreach (var arg in args)
+            {
+                var paramValues = arg.GetType().GetProperties()
+                    .Select(p => p.GetValue(arg)?.ToString() ?? string.Empty);
+                sb.Append(string.Join('_', paramValues));
+            }
+
+            return sb.ToString();
+        }
     }
-
-    public override void Intercept(IInvocation invocation)
-    {
-      var methodName = string.Format($"{invocation.Arguments[0]}.{invocation.Method.Name}");
-      var arguments = invocation.Arguments;
-      var key = $"{methodName}({BuildKey(arguments)})";
-      if (_cacheManager.IsAdd(key))
-      {
-        invocation.ReturnValue = _cacheManager.Get(key);
-        return;
-      }
-
-      invocation.Proceed();
-      _cacheManager.Add(key, invocation.ReturnValue, _duration);
-    }
-
-
-    string BuildKey(object[] args)
-    {
-      var sb = new StringBuilder();
-      foreach (var arg in args)
-      {
-        var paramValues = arg.GetType().GetProperties()
-            .Select(p => p.GetValue(arg)?.ToString() ?? string.Empty);
-        sb.Append(string.Join('_', paramValues));
-      }
-
-      return sb.ToString();
-    }
-  }
 }
