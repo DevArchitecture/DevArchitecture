0418b9f857e176991a726d526b82696cc22a26a3
Kerem VARIŞ <keremvaris@hotmail.com.tr>
2022-02-28 23:06:23 +0300

    .

--- UiPreparation/UI/src/environments/environment.ts
+++ UiPreparation/UI/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  getApiUrl: "https://localhost:44375/WebAPI/api",
+  getApiUrl: "https://localhost:5001/api",
   getDropDownSetting: {
     singleSelection: false,
     idField: 'id',
