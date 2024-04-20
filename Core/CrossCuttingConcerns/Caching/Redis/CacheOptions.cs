4808ad2e2780c0b02c89bc8c1a034ba633990973
Muzaffer AKYIL <makyil@gmail.com>
2024-04-20 06:08:02 +0300

    replaced servicestack.redis package with stackexchange.redis & added cache options

---
+++ Core/CrossCuttingConcerns/Caching/Redis/CacheOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.CrossCuttingConcerns.Caching.Redis
+{
+    public class CacheOptions
+    {
+        public string Host { get; set; }
+        public string Password { get; set; }
+        public int Database { get; set; }
+        public int Port { get; set; }
+    }
+}
---
+++ Tests/Core/CrossCuttingConcerns/CacheTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using FluentAssertions;
+using NUnit.Framework;
+using Tests.Helpers.Token;
+
+namespace Tests.Core.CrossCuttingConcerns
+{
+    [TestFixture]
+    public class CacheTests : BaseIntegrationTest
+    {
+        [Test]
+        public void CacheTest()
+        {
+            var cacheManager = new MemoryCacheManager();
+            var key = $"TestCache_{Guid.NewGuid()}";
+            var first = cacheManager.Get<string>(key);
+            first.Should().BeNull();
+            cacheManager.IsAdd(key).Should().BeFalse();
+
+            cacheManager.Add(key, "test", 1);
+
+            var second = cacheManager.Get<string>(key);
+            second.Should().NotBeNull();
+            second.Should().Be("test");
+            cacheManager.IsAdd(key).Should().BeTrue();
+
+            cacheManager.Remove(key);
+            cacheManager.IsAdd(key).Should().BeFalse();
+        }
+    }
+}
