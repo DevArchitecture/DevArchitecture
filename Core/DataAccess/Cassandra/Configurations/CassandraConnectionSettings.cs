ed64f643f9eeb04e101267d4e617b038d51155c2
Burak Halefoğlu <burakhalefoglu@gmail.com>
2024-06-29 09:02:08 +0300

    refact

--- Core/DataAccess/Cassandra/Configurations/CassandraConnectionSettings.cs
+++ Core/DataAccess/Cassandra/Configurations/CassandraConnectionSettings.cs
@@ -5,6 +5,5 @@ public class CassandraConnectionSettings
     public string Host { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
-
     public string Keyspace { get; set; }
 }
