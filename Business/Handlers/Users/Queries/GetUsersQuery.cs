eda904bbe4d223e44e85bc0a9c4eb43e631dfda5
Kerem VARIŞ <kerem.varis@carbonance.com>
2026-06-24 15:54:33 +0300

    feat(backend): integrate pagination into user queries

--- Business/Handlers/Users/Queries/GetUsersQuery.cs
+++ Business/Handlers/Users/Queries/GetUsersQuery.cs
@@ -8,6 +8,7 @@ using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Logging;
 using Core.Aspects.Autofac.Performance;
 using Core.CrossCuttingConcerns.Logging.Serilog.Loggers;
+using Core.Entities;
 using Core.Entities.Dtos;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -15,9 +16,12 @@ using MediatR;
 
 namespace Business.Handlers.Users.Queries
 {
-    public class GetUsersQuery : IRequest<IDataResult<IEnumerable<UserDto>>>
+    public class GetUsersQuery : BasePaginatedQuery<UserDto>
     {
-        public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, IDataResult<IEnumerable<UserDto>>>
+        public GetUsersQuery(int pageNumber = 1, int pageSize = 10) 
+            : base(pageNumber, pageSize) { }
+        
+        public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, IDataResult<PaginatedResult<IEnumerable<UserDto>>>>
         {
             private readonly IUserRepository _userRepository;
             private readonly IMapper _mapper;
@@ -32,12 +36,27 @@ namespace Business.Handlers.Users.Queries
             [PerformanceAspect(5)]
             [CacheAspect(10)]
             [LogAspect(typeof(FileLogger))]
-            public async Task<IDataResult<IEnumerable<UserDto>>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+            public async Task<IDataResult<PaginatedResult<IEnumerable<UserDto>>>> Handle(
+                GetUsersQuery request, CancellationToken cancellationToken)
             {
                 var userList = await _userRepository.GetListAsync();
-                var userDtoList = userList.Select(user => _mapper.Map<UserDto>(user)).ToList();
+                var totalCount = userList.Count();
+                
+                var paginatedUsers = userList
+                    .Skip((request.PageNumber - 1) * request.PageSize)
+                    .Take(request.PageSize)
+                    .Select(user => _mapper.Map<UserDto>(user))
+                    .ToList();
 
-                return new SuccessDataResult<IEnumerable<UserDto>>(userDtoList);
+                var result = new PaginatedResult<IEnumerable<UserDto>>(
+                    paginatedUsers, 
+                    request.PageNumber, 
+                    request.PageSize);
+                
+                result.TotalRecords = totalCount;
+                result.TotalPages = (int)System.Math.Ceiling(totalCount / (double)request.PageSize);
+
+                return new SuccessDataResult<PaginatedResult<IEnumerable<UserDto>>>(result);
             }
         }
     }
